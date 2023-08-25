5bf8b6820fd2687149e17a6bcf903b203ad7bffe
Joachim Breitner <mail@joachim-breitner.de>
2023-08-25 11:15:40 +0200

    No ProofWidgets for now

--- flake.nix
+++ flake.nix
@@ -5,8 +5,6 @@
   inputs.nixpkgs.follows = "lean/nixpkgs";
 
   inputs.mathlib4.url = "github:leanprover-community/mathlib4/joachim/find-no-ProofWidgets";
-  #inputs.mathlib4.url = "git+file:///home/jojo/build/lean/mathlib4?rev=069e0083ff4f1081710a73ec243e66d659ae59d5";
-  #inputs.mathlib4.url = "github:leanprover-community/mathlib4/joachim/find";
   inputs.mathlib4.flake = false;
   inputs.std4.url = "github:leanprover/std4/8b864260672b21d964d79ecb2376e01d0eab9f5b";
   inputs.std4.flake = false;
@@ -14,8 +12,10 @@
   inputs.quote4.flake = false;
   inputs.aesop.url = "github:nomeata/JLimpberg/d13a9666e6f430b940ef8d092f1219e964b52a09";
   inputs.aesop.flake = false;
-  inputs.ProofWidgets.url = "github:EdAyers/ProofWidgets4/a0c2cd0ac3245a0dade4f925bcfa97e06dd84229";
-  inputs.ProofWidgets.flake = false;
+
+  # inputs.ProofWidgets.url = "github:EdAyers/ProofWidgets4/a0c2cd0ac3245a0dade4f925bcfa97e06dd84229";
+  # inputs.ProofWidgets.flake = false;
+
   outputs = { self, nixpkgs, ...}@inputs:
     let
       system = "x86_64-linux";
@@ -40,23 +40,30 @@
         deps = [std4];
       };
 
-      # ProofWidgets_flake = lake2nix.lib.lakeRepo2flake {
+      # ProofWidgets = inputs.lean.packages.${system}.buildLeanPackage {
       #   name = "ProofWidgets";
-      #   lean = lake2nix.inputs.lean;
       #   src = inputs.ProofWidgets;
-      #   depFlakes = [std4_flake];
+      #   roots = [ "ProofWidgets" ];
+      #   deps = [std4];
       # };
 
       mathlib4 = inputs.lean.packages.${system}.buildLeanPackage {
         name = "Mathlib";
         src = inputs.mathlib4;
+        # src = builtins.fetchTree {
+        #   name = "mathlib4-patched";
+        #   src = inputs.mathlib4;
+        #   postPatch = ''
+        #     sed -i '/Widget/d' ./Mathlib.lean ./Mathlib/Tactic.lean
+        #   '';
+        # };
         roots = [ { mod = "Mathlib"; glob = "one"; } ];
         leanFlags = [
           "-Dpp.unicode.fun=true"
           "-DautoImplicit=false"
           "-DrelaxedAutoImplicit=false"
         ];
-        deps = [std4 quote4 aesop];
+        deps = [std4 quote4 aesop ];
       };
 
       loogle = inputs.lean.packages.${system}.buildLeanPackage {
