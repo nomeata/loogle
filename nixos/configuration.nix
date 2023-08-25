4f23e5083978a7f0722531a8ad03c3bc8bf93415
Joachim Breitner <mail@joachim-breitner.de>
2023-08-25 17:00:34 +0200

    Set up for hetzner

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,17 +1,6 @@
 { config, pkgs, modulesPath, lib, environment, loogle_server, ... }:
 {
-  imports = [
-    (modulesPath + "/virtualisation/digital-ocean-config.nix")
-    # (modulesPath + "/virtualisation/qemu-vm.nix")
-    ./ship.nix
-  ];
-
-  nix.settings.substituters = [
-    "https://cache.garnix.io"
-  ];
-  nix.settings.trusted-public-keys = [
-    "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-  ];
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
   # for testing
   # virtualisation.forwardPorts = [
@@ -19,6 +8,29 @@
   # ];
   # virtualisation.memorySize = 2048;
   # users.users.root.initialPassword = "test";
+
+  boot.loader.grub.device = "/dev/sda";
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
+  boot.initrd.kernelModules = [ "nvme" ];
+  fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
+
+  boot.tmp.cleanOnBoot = true;
+  zramSwap.enable = true;
+  networking.hostName = "loogle";
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJRd0CZZQXyKTEQSEtrIpcTg15XEoRjuYodwo0nr5hNj jojo@kirk"
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBMYx13rpT1E87lw5yNyRs1Lq3/vwd3fxjRwq9nJz4b3iVSAGXnwUxDVbi3m2H1NSNCsoOFOVej+yPMkmIs/+Wxo= pixel-tpm"
+  ];
+
+  nix.settings.substituters = [
+    "https://cache.nixos.org/"
+    "https://cache.garnix.io"
+  ];
+  nix.settings.trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+  ];
 
 
   documentation.nixos.enable = false;
