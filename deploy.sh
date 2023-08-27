9a2f26c0497091c61ac1fa3d7cf60bba7f4cb4fb
Joachim Breitner <mail@joachim-breitner.de>
2023-08-27 10:07:59 +0200

    GC after deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 nixos-rebuild switch --use-substitutes --target-host root@loogle.nomeata.de  --flake .#loogle --fast
+ssh root@loogle.nomeata.de profile wipe-history --profile /nix/var/nix/profiles/system
+ssh root@loogle.nomeata.de nix store gc
