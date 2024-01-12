fe1074d77b0267601aa594526805b762e4308024
Joachim Breitner <mail@joachim-breitner.de>
2024-01-12 10:56:54 +0100

    Update mathlib

--- flake.nix
+++ flake.nix
@@ -8,13 +8,13 @@
   inputs.nixpkgs.url = github:NixOS/nixpkgs;
   inputs.nixpkgs.follows = "lean/nixpkgs";
 
-  inputs.mathlib4.url = "github:leanprover-community/mathlib4/03bf774359b9e2b8ea4f21537284b015731dee35";
+  inputs.mathlib4.url = "github:leanprover-community/mathlib4/c1cd6459c24cdd591e94efe695e2256a28974ba5";
   inputs.mathlib4.flake = false;
-  inputs.std.url = "github:leanprover/std4/0f6bc5b32bf5b0498902d3b5f0806c75530539d5";
+  inputs.std.url = "github:leanprover/std4/0b2e962a80b98df1c6455f8ae11f2bd16809067c";
   inputs.std.flake = false;
   inputs.quote4.url = "github:gebner/quote4/1c88406514a636d241903e2e288d21dc6d861e01";
   inputs.quote4.flake = false;
-  inputs.aesop.url = "github:JLimperg/aesop/69404390bdc1de946bf0a2e51b1a69f308e56d7a";
+  inputs.aesop.url = "github:JLimperg/aesop/2ae78a474ddf0a39bc2afb9f9f284d2e64f48a70";
   inputs.aesop.flake = false;
   inputs.ProofWidgets.url = "github:EdAyers/ProofWidgets4/8dd18350791c85c0fc9adbd6254c94a81d260d35";
   inputs.ProofWidgets.flake = false;
