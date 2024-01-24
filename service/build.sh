e1213915cd57909b4ccdd32c5d78989923785b55
Joachim Breitner <mail@joachim-breitner.de>
2024-01-24 17:35:56 +0100

    Draft a new nix setup for lake-based loogle deployment

---
+++ service/build.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+# Usage: $0 dest/
+#
+# This script
+# * creates a temporary directory in dest/
+# * fetches the latest loogle
+# * runs lake update to get the latest mathlib
+# * builds the loogle binary and the mathlib cache
+# * does a quick sanity check
+# * atomically replaces the dest/live symlink
+# * if successful, delets all other directories
+
+set -e
+
+DEST="$1"
+if [ -z "$DEST" ]; then
+    echo "Usage $0 dest/"
+    exit 1
+fi
+
+mkdir -p "$DEST"
+cd "$DEST"
+
+workdir="deploy-$(date --iso=seconds)"
+logfile="$workdir.log"
+exec &> >(tee -a "$logfile")
+
+echo "Working in $DEST/$workdir"
+mkdir -p "$workdir"
+
+echo "Cloning loogle"
+git clone --depth=1 https://github.com/nomeata/loogle.git "$workdir"
+cd "$workdir"
+
+echo "Replace toolchain by mathlib's"
+curl -L https://raw.githubusercontent.com/leanprover-community/mathlib4/master/lean-toolchain -o lean-toolchain
+cat lean-toolchain
+rm ./lake-manifest.json
+
+echo "Updating lake"
+export LOOGLE_SECCOMP=true
+lake update
+lake exe cache get
+
+echo "Building loogle"
+lake build loogle
+
+echo "Building mathlib cache"
+lake build LoogleMathlibCache
+
+echo "Sanity checks"
+echo "Does .extra exist?"
+test -e ./.lake/build/lib/LoogleMathlibCache.extra
+echo "Does the binary exist?"
+test -x ./.lake/build/bin/loogle
+
+echo "Does it run"
+./.lake/build/bin/loogle
+
+echo "Does it work (fast enough, so using the cache)"
+timeout 10s ./.lake/build/bin/loogle List.replicate|grep -q 'List.replicate (from Init.Data.List.Basic)'
+
+echo "Size of .lake"
+ls -sh .lake
+
+echo "All good, going live"
+cd ..
+
+rm -f tmp
+ln -s "$workdir" tmp
+mv -T tmp live
+
+for file in deploy-*; do
+  if [ "$file" != "$workdir" ] && [ "$file" != "$logfile" ]; then
+    echo "Deleting old build $file"
+    rm -rf "$file"
+  fi
+done
+
+echo "All done"
---
+++ service/configuration.nix
@@ -0,0 +1,190 @@
+{ config, self, pkgs, modulesPath, lib, environment, nixpkgs, ... }:
+let
+  # a bit of a hack
+  inVM = config.networking.dhcpcd.extraConfig == "noarp";
+in {
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+
+  # for testing
+  virtualisation.vmVariant = {
+    virtualisation.forwardPorts = [
+      { from = "host"; host.port = 8888; guest.port = 80; }
+    ];
+    virtualisation.memorySize = 8096;
+    virtualisation.diskSize = 50000;
+    users.users.root.initialPassword = "test";
+  };
+
+  boot.loader.grub.device = "/dev/sda";
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
+  boot.initrd.kernelModules = [ "nvme" ];
+  fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
+
+  boot.tmp.cleanOnBoot = true;
+  zramSwap.enable = true;
+  networking.hostName = "loogle";
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJRd0CZZQXyKTEQSEtrIpcTg15XEoRjuYodwo0nr5hNj jojo@kirk"
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBMYx13rpT1E87lw5yNyRs1Lq3/vwd3fxjRwq9nJz4b3iVSAGXnwUxDVbi3m2H1NSNCsoOFOVej+yPMkmIs/+Wxo= pixel-tpm"
+  ];
+
+  nix.settings.substituters = [
+    "https://cache.nixos.org/"
+    "https://cache.garnix.io"
+  ];
+  nix.settings.trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+  ];
+
+  # enable nix flakes
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.registry.nixpkgs.flake = nixpkgs;
+  nix.nixPath = [
+    "nixpkgs=/etc/nixpkgs/channels/nixpkgs}"
+    "/nix/var/nix/profiles/per-user/root/channels"
+  ];
+  systemd.tmpfiles.rules = [
+    "L+ /etc/nixpkgs/channels/nixpkgs     - - - - ${nixpkgs}"
+  ];
+
+  documentation.nixos.enable = false;
+  documentation.enable = false;
+
+  environment.systemPackages = [
+    self.packages.${pkgs.system}.loogle-updater
+  ];
+
+  security.acme.defaults.email = "mail@joachim-breitner.de";
+  security.acme.acceptTerms = true;
+
+  services.openssh = {
+    enable = true;
+    ports = [ 2722 ];
+    settings.PasswordAuthentication = false;
+  };
+
+  services.nginx = {
+    enable = true;
+    enableReload = true;
+    recommendedProxySettings = true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedTlsSettings = true;
+    proxyTimeout = "300s";
+  };
+
+  services.nginx.virtualHosts = {
+    "loogle.lean-lang.org" = {
+      enableACME = true;
+      default = true;
+      forceSSL = !inVM;
+      locations = {
+        "/" = {
+          proxyPass = "http://localhost:8080";
+        };
+      };
+    };
+    "loogle.lean-fro.org" = {
+      enableACME = true;
+      forceSSL = !inVM;
+      globalRedirect = "loogle.lean-lang.org";
+    };
+    "loogle.nomeata.de" = {
+      enableACME = true;
+      forceSSL = !inVM;
+      globalRedirect = "loogle.lean-lang.org";
+    };
+  };
+
+  users.users.loogle = {
+    isNormalUser = true;
+  };
+
+  systemd.services.loogle = {
+    description = "Loogle";
+    enable = true;
+    after = [
+      "network.target"
+    ];
+    wantedBy = [
+      "multi-user.target"
+    ];
+    path = [
+      (pkgs.python3.withPackages(p: with p; [prometheus-client]))
+      pkgs.git
+    ];
+    serviceConfig = {
+      Type = "simple";
+      User = "loogle";
+      Restart = "always";
+      ConditionPathExists = "/home/loogle/deploy/live";
+      WorkingDirectory = "/home/loogle/deploy/live";
+      ExecStart = "/home/loogle/deploy/live/server.py";
+      NoNewPrivileges = true;
+      ProtectSystem = "strict";
+      ProtectHome = "read-only";
+    };
+  };
+
+  # inspired by https://superuser.com/a/1276457
+  systemd.services.loogle-watcher = {
+    description = "Loogle file watcher";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart="systemctl restart loogle.service";
+    };
+  };
+  systemd.paths.loogle-watcher = {
+    wantedBy = [ "multi-user.target" ];
+    pathConfig = {
+      PathChanged = [
+        "/home/loogle/deploy/live"
+      ];
+    };
+  };
+
+  systemd.services.loogle-updater = {
+    wants = ["network-online.target"];
+    after = ["network-online.target"];
+
+    path = with pkgs; [ elan git ];
+
+    startAt = "00/6:00"; #  repeat every 6 hours
+    serviceConfig = {
+      User = "loogle";
+      WorkingDirectory = "~";
+      ExecStart = "${self.packages.${pkgs.system}.loogle-updater}/bin/loogle-updater /home/loogle/deploy";
+#      Restart = "on-failure";
+      NoNewPrivileges = true;
+      ProtectSystem = "strict";
+      PrivateTmp = true;
+    };
+    unitConfig = {
+      StartLimitIntervalSec = "10";
+      StartLimitBurst = "3";
+    };
+  };
+
+  swapDevices = [{ device = "/swapfile"; size = 2048; }];
+
+  nix.settings.sandbox = false;
+
+  # Automatic garbage collection. Enabling this frees up space by removing unused builds periodically
+  nix.gc = {
+    automatic = false;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
+  networking.firewall.enable = true;
+  networking.firewall.allowedTCPPorts = [ 80 443 22 ];
+
+  services.journald.extraConfig = "SystemMaxUse=100M";
+
+  programs.vim.defaultEditor = true;
+
+  services.fail2ban.enable = true;
+  system.stateVersion = "22.11";
+}
