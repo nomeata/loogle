1b090fdcaaf8009fc77dcf2ed7603b1735201fc4
Joachim Breitner <mail@joachim-breitner.de>
2024-01-30 10:06:15 +0100

    Delete old builds, to avoid out of memory

--- service/build.sh
+++ service/build.sh
@@ -22,6 +22,17 @@ fi
 mkdir -p "$DEST/build" "$DEST/deploy"
 cd "$DEST"
 
+echo "Cleaning up $DEST/build"
+cd build/
+old_live="$(readlink ../deploy/live)"
+echo "Currently live: $old_live"
+for file in deploy-*; do
+  if [ "../build/$file" != "$old_live" ] && [ "../build/$file" != "$old_live.log" ]; then
+    echo "Deleting old build $file"
+    rm -rf "$file"
+  fi
+done
+
 workdir="deploy-$(date --iso=seconds)"
 logfile="$workdir.log"
 exec &> >(tee -a "build/$logfile")
