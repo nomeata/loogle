31f816c2af1548a4753caf2688906a9c670bd697
Joachim Breitner <mail@joachim-breitner.de>
2024-02-07 22:34:10 +0100

    Experiment with garnix

--- flake.nix
+++ flake.nix
@@ -1,13 +1,15 @@
 {
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
+  inputs.service.url =  "path:./service"; # edit: use url path syntax
 
-  outputs = { self, nixpkgs, ...}@inputs:
+  outputs = { self, nixpkgs, service, ...}@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       my_python = pkgs.python3.withPackages(p: with p; [prometheus-client]);
     in
     {
+      nixosConfigurations = service.nixosConfigurations;
       devShells.${system}.default = (pkgs.mkShell.override { stdenv = pkgs.llvmPackages_15.stdenv; }) {
         packages = with pkgs;
           [ elan
