06898623cafbf5842c4f79165880926f28bce85b
Joachim Breitner <mail@joachim-breitner.de>
2024-02-07 22:38:56 +0100

    Copy flake.nix

--- flake.nix
+++ flake.nix
@@ -1,15 +1,44 @@
 {
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
-  inputs.service.url =  "path:./service"; # edit: use url path syntax
 
-  outputs = { self, nixpkgs, service, ...}@inputs:
+  outputs = { self, nixpkgs, ...}@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       my_python = pkgs.python3.withPackages(p: with p; [prometheus-client]);
     in
     {
-      nixosConfigurations = service.nixosConfigurations;
+      packages.${system} = {
+        loogle-updater =
+          let deps = [
+            pkgs.gitMinimal
+            pkgs.elan
+            pkgs.coreutils
+            pkgs.curl
+            pkgs.pkg-config
+            pkgs.llvmPackages_15.clang
+            pkgs.gnugrep
+            pkgs.gnutar
+            pkgs.gzip
+          ]; in
+          pkgs.runCommand "loogle-updater"
+            { nativeBuildInputs = [ pkgs.makeWrapper ]; }
+            ''
+              install -m755 ${./service/build.sh} -D $out/bin/loogle-updater
+              patchShebangs $out/bin/loogle-updater
+              wrapProgram $out/bin/loogle-updater \
+                --set PATH ${pkgs.lib.makeBinPath deps } \
+                --prefix NIX_CFLAGS_COMPILE_x86_64_unknown_linux_gnu " " "-I${pkgs.pkgsStatic.libseccomp.dev}/include" \
+                --prefix NIX_LDFLAGS_x86_64_unknown_linux_gnu " " "-L${pkgs.pkgsStatic.libseccomp.lib}/lib" \
+            '';
+      };
+
+      nixosConfigurations.loogle = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit nixpkgs self; };
+        modules = [ ./service/configuration.nix ];
+      };
+
       devShells.${system}.default = (pkgs.mkShell.override { stdenv = pkgs.llvmPackages_15.stdenv; }) {
         packages = with pkgs;
           [ elan
@@ -19,3 +48,4 @@
       };
     };
 }
+
