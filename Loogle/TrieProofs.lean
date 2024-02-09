dbe5f397935a5567918a3e2867d9b23a75a90d2d
Joachim Breitner <mail@joachim-breitner.de>
2024-02-09 19:24:44 +0100

    Try to use wfinduct

--- Loogle/TrieProofs.lean
+++ Loogle/TrieProofs.lean
@@ -1,4 +1,5 @@
 import Loogle.Trie
+import WfInduct.Basic
 import Std.Data.ByteArray
 
 set_option autoImplicit false
@@ -89,92 +90,106 @@ theorem valid_loop_insertEmpty {α} (i : Nat) (k : String) (f : Option α → α
   apply valid_mkPath
   apply Trie.valid.leaf
 
-theorem valid_loop_upsert {α} (t : Trie α) (i : Nat) (k : String) (f : Option α → α) (h : Trie.valid t):
+#derive_induction Trie.upsert.loop
+
+theorem valid_loop_upsert {α} (t : Trie α) (i : Nat) (k : String) (f : Option α → α) (ht : Trie.valid t):
     Trie.valid (Trie.upsert.loop k f i t) := by
-  cases h with
-  | leaf v =>
-    simp only [Trie.upsert.loop]; simp_utf8
+  induction i, t using Trie.upsert.loop.induct (s := k) (f := f)
+  case case1 i v hi =>
+    simp only [Trie.upsert.loop, hi, if_pos]; simp_utf8
+    apply valid_mkPath; apply Trie.valid.leaf
+  case case2 i v hi =>
+    simp only [Trie.upsert.loop, hi, if_neg]; simp_utf8
+    dsimp -- TODO: how to cleanly resolve if
+    apply Trie.valid.leaf
+  case case3 i v ps hps t' hi j hj =>
+    intro ht -- TODO: Why do we have to intro this here?
+    cases ht; case path _ ht =>
+    simp only [Trie.upsert.loop, hi, hj, if_pos, if_neg]; simp_utf8
+    dsimp
+    apply valid_mkPath
+    apply IH1
+    apply valid_mkPath
+    exact ht
+  case case4 i v ps hps t hi =>
+    cases ht; case path _ ht =>
+    simp only [Trie.upsert.loop, hi, if_pos, if_neg]; simp_utf8; dsimp
     split
-    case inr hi => apply Trie.valid.leaf
-    case inl hi => apply valid_mkPath; apply Trie.valid.leaf
-  | path v ps hps t ht =>
-    simp only [Trie.upsert.loop]; simp_utf8
+    case inl hp =>
+      apply valid_mkPath
+      sorry -- apply IH -- Bug in WfInduct?
+      -- apply valid_mkPath
+      -- exact h
+    case inr hp =>
+      apply Trie.valid.node
+      · simp [ByteArray.size]
+      · have heq0 : Trie.commonPrefix k ps i = 0 := by omega
+        have := Trie.commonPrefix_differs_head k ps i heq0 hi hps
+        simpa [List.Nodup]
+      · simp only [List.mem_toArray, List.mem_cons, List.mem_singleton, forall_eq_or_imp, forall_eq]
+        constructor
+        case left =>
+          apply valid_loop_insertEmpty
+        case right =>
+          simp only [List.not_mem_nil, false_implies, forall_const, and_true]
+          apply valid_mkPath
+          exact ht
+  case case5 i v ps hps t' hi =>
+    cases ht; case path _ ht =>
+    simp only [Trie.upsert.loop, hi, if_pos, if_neg]; simp_utf8; dsimp
+    constructor; assumption
+  case case6 i v cs ts hi IH =>
+    cases ht; case node hnodup hts hsize =>
+    simp only [Trie.upsert.loop, hi, if_pos, if_neg]; simp_utf8; dsimp
+    rw [ByteArray.findIdx_list]
     split
-    case inr hi => apply Trie.valid.path; assumption
-    case inl hi =>
-      split
-      case inl hp =>
-        apply valid_mkPath
-        apply valid_loop_upsert -- induction happens here
-        apply valid_mkPath
-        exact ht
-      case inr hp =>
-        apply Trie.valid.node
-        · simp [ByteArray.size]
-        · have heq0 : Trie.commonPrefix k ps i = 0 := by omega
-          have := Trie.commonPrefix_differs_head k ps i heq0 hi hps
-          simpa [List.Nodup]
-        · simp only [List.mem_toArray, List.mem_cons, List.mem_singleton, forall_eq_or_imp, forall_eq]
-          constructor
-          case left =>
-            apply valid_loop_insertEmpty
-          case right =>
-            simp only [List.not_mem_nil, false_implies, forall_const, and_true]
-            apply valid_mkPath
-            exact ht
-  | node v cs ts hsize hdisitnct hts =>
-    simp only [Trie.upsert.loop]; simp_utf8
-    split
-    case inr hi => apply Trie.valid.node <;> assumption
-    case inl hi =>
-      rw [ByteArray.findIdx_list]
-      split
-      case h_1 _ hfindnone =>
-        apply Trie.valid.node
-        · simpa
-        · simp only [ByteArray.toList_push, List.nodup_snoc, hdisitnct, and_true]
-          simp only [List.mem_iff_get?, not_exists]
-          intro j hj
-          have := List.findIdx?_of_eq_none hfindnone j
-          rw [hj] at this
-          simp at this
-          done
-        · simp only [← Array.mem_data, Array.push_data, List.mem_append, List.mem_singleton]
-          simp only [Array.mem_data]
-          intro t ht
-          cases ht with
-          | inl ht => exact hts t ht
-          | inr ht => cases ht; apply valid_loop_insertEmpty
-      case h_2 _ idx _hfindsome =>
-        apply Trie.valid.node
-        · simpa
-        · assumption
-        · simp only [← Array.mem_data, List.mem_iff_get?, forall_exists_index]
+    case h_1 _ hfindnone =>
+      apply Trie.valid.node
+      · simpa
+      · simp only [ByteArray.toList_push, List.nodup_snoc, hnodup, and_true]
+        simp only [List.mem_iff_get?, not_exists]
+        intro j hj
+        have := List.findIdx?_of_eq_none hfindnone j
+        rw [hj] at this
+        simp at this
+        done
+      · simp only [← Array.mem_data, Array.push_data, List.mem_append, List.mem_singleton]
+        simp only [Array.mem_data]
+        intro t ht
+        cases ht with
+        | inl ht => exact hts t ht
+        | inr ht => cases ht; apply valid_loop_insertEmpty
+    case h_2 _ idx _hfindsome =>
+      apply Trie.valid.node
+      · simpa
+      · assumption
+      · simp only [← Array.mem_data, List.mem_iff_get?, forall_exists_index]
+        simp only [← Array.get?_eq_data_get?]
+        simp only [Array.get?_modify]
+        intro t' j
+        split
+        case inl hj =>
+          cases hj
+          simp only [Option.map_eq_some', forall_exists_index, and_imp]
+          intro t'' ht'' ht''
+          cases ht''
+          apply IH -- induction happens here
+          apply hts
+          simp only [← Array.mem_data, List.mem_iff_get?]
           simp only [← Array.get?_eq_data_get?]
-          simp only [Array.get?_modify]
-          intro t' j
-          split
-          case inl hj =>
-            cases hj
-            simp only [Option.map_eq_some', forall_exists_index, and_imp]
-            intro t'' ht'' ht''
-            cases ht''
-            apply valid_loop_upsert -- induction happens here
-            apply hts
-            simp only [← Array.mem_data, List.mem_iff_get?]
-            simp only [← Array.get?_eq_data_get?]
-            exact ⟨_, ht''⟩
-          case inr hj =>
-            intro ht''
-            apply hts
-            simp only [← Array.mem_data, List.mem_iff_get?]
-            simp only [← Array.get?_eq_data_get?]
-            exact ⟨_, ht''⟩
-            done
-termination_by _ => k.toUTF8.size - i
-decreasing_by
-  simp_wf
-  omega
+          exact ⟨_, ht''⟩
+        case inr hj =>
+          intro ht''
+          apply hts
+          simp only [← Array.mem_data, List.mem_iff_get?]
+          simp only [← Array.get?_eq_data_get?]
+          exact ⟨_, ht''⟩
+          done
+  case case7 i v cs ts hi =>
+    cases ht; case node hnodup hts hsize =>
+    simp only [Trie.upsert.loop, hi, if_pos, if_neg]; simp_utf8; dsimp
+    apply Trie.valid.node <;> assumption
+
 
 theorem valid_upsert {α} (t : Trie α) (k : String) (f : Option α → α) (h : Trie.valid t):
   Trie.valid (t.upsert k f) := valid_loop_upsert _ _ _ _ h
